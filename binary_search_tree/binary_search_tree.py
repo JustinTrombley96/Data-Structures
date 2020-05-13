04df686ecc48cd2ca883d9bb2e6104b85897a2fe
Justin Trombley <jtrombley96@gmail.com>
2020-05-13 17:29:43 -0400

    Contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -22,7 +22,24 @@ class BSTNode:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # when we start searching, self will be the root
+        # compare the target against self
+        #
+        # Criteria for returning False: 
+        # We know we need to go in one direction
+        # but there is nothing in the left or right direction
+        if target == self.value:
+            return True
+        if target < self.value:
+            # go left if left is a BSTNode
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            # go right if right is a BSTNode
+            if not self.right:
+                return False
+            return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
